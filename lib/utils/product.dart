0a294a061172dcd42c8e4d8665241e8aae486309
nascimento99 <welliana.00000850279@unicap.br>
2024-12-09 13:39:00 -0300

    feat: product

---
+++ lib/utils/product.dart
@@ -0,0 +1,8 @@
+// lib/utils/product.dart
+class Product {
+  final String imageUrl;
+  final String name;
+  final double price;
+
+  Product({required this.imageUrl, required this.name, required this.price});
+}
